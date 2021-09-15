1fca31cc50de278787df7f5abc78e7e1155be316
intertxt <intertxt@protonmail.com>
2021-09-15 16:18:20 +0200

    Michelle: Redundant char removal

--- bl_gerichte_scraper.py
+++ bl_gerichte_scraper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-# scraper for AG_Gerichte
+# scraper for BL_Gerichte
 
 from bs4 import BeautifulSoup
 import os
@@ -23,7 +23,7 @@ PATH_TO_DATA = args.path_to_data
 
 SAVE_PATH = '/home/admin1/tb_tool/clean_scraper_data/BL_Gerichte_clean/'
 
-absatz_pattern = r'^[0-9]+\.([0-9]+(\.)?)*(\s-\s[0-9]+\.([0-9]+(\.)?)*)?'
+absatz_pattern = r'^(\s)?[0-9]+\.([0-9]+(\.)?)*(\s-\s[0-9]+\.([0-9]+(\.)?)*)?'
 datum_pattern = r'[0-9][0-9]?\.[\s]{1,2}([A-Z][a-z]+|März)\s[1-9][0-9]{3}'
 
 
@@ -31,18 +31,40 @@ def parse_text(parsed_html) -> List[str]:
 	"""Get text out of HTML-files."""
 	invalid_tags = ['sup']
 	text = []
-	for tag in parsed_html.findAll(["p", 'td', 'strong']):
-		for child in tag.findAll(invalid_tags):
-			child.decompose()
-		text.append(unicodedata.normalize('NFKD', tag.get_text()).strip('\n        ').replace('\n         \n         ', ' '))
+	if parsed_html.findAll(['p']):
+		for tag in parsed_html.findAll(["p", 'td', 'strong']):
+			for child in tag.findAll(invalid_tags):
+				child.decompose()
+			tag_text = unicodedata.normalize('NFKD', tag.get_text()).strip('\n        ').replace('\n         \n         ', ' ')
+			tag_text = tag_text.strip(' ')
+			tag_text = tag_text.strip('\n')
+			text.append(tag_text)
+	else:
+		for tag in parsed_html.findAll(['div']):
+			for child in tag.findAll(invalid_tags):
+				child.decompose()
+			tag_text = unicodedata.normalize('NFKD', tag.get_text()).strip('\n        ').replace('\n         \n         ', ' ')
+			tag_text = tag_text.strip(' ')
+			tag_text = tag_text.strip('\n')
+			text.append(tag_text)
 	return text
 
 
 def remove_hyphens(old_text) -> List[str]:
 	"""Removes hyphens and merge elements."""
 	text_wo_hyphens = []
-	text = ''
 	for i, para in enumerate(old_text):
+		para = para.replace('        ', ' ')
+		para = para.replace('       ', ' ')
+		para = para.replace('      ', ' ')
+		para = para.replace('     ', ' ')
+		para = para.replace('    ', ' ')
+		para = para.replace('   ', ' ')
+		para = para.replace('  ', ' ')
+		para = para.replace('\n\n\n\n', ' ')
+		para = para.replace('\n\n\n', ' ')
+		para = para.replace('\n\n', ' ')
+		para = para.replace('\n', ' ')
 		if para.endswith('-') and old_text[i+1][0].islower:
 			text_wo_hyphens.append(para[:-1]+old_text[i+1])
 			del old_text[i+1]
@@ -53,24 +75,26 @@ def remove_hyphens(old_text) -> List[str]:
 
 def remove_page_breaks(text_wo_hyphens) -> List[str]:
 	""""Remove page breaks and merge elements."""
-	# text_list2 = []
-	# text2 = ''
-	# for i, para in enumerate(text_list):
-	# 	if i+1 < len(text_list) and para[:-1] not in '.!?:)0987654321':
-	# 		text_list2.append(para + ' ' + text_list[i + 1])
-	# 		del text_list[i+1]
-	# 	else:
-	# 		text_list2.append(para)
-	pass
+	clean_text_list = []
+	for i, element in enumerate(text_wo_hyphens):
+		if i+1 < len(text_wo_hyphens) and text_wo_hyphens[i+1][0].islower():
+			clean_text_list.append(element + ' ' + text_wo_hyphens[i+1])
+			del text_wo_hyphens[i+1]
+		elif i+1 < len(text_wo_hyphens) and text_wo_hyphens[i+1].startswith('X.-Weg'):
+			clean_text_list.append(element + ' ' + text_wo_hyphens[i+1])
+			del text_wo_hyphens[i+1]
+		else:
+			clean_text_list.append(element)
+	return clean_text_list
 
 
 def split_absatznr(text_list) -> List[str]:
 	"""Split 'Absatznummern' from rest of the text."""
 	paragraph_list = []
 	for i, element in enumerate(text_list):
-		if re.match(absatz_pattern, element):
-			match = re.match(absatz_pattern, element).group(0)
-			if element.startswith(match) and element.endswith(match):
+		if re.search(absatz_pattern, element):
+			match = re.search(absatz_pattern, element).group(0)
+			if re.fullmatch(absatz_pattern, element):
 				paragraph_list.append(element)
 			elif element.startswith(match):
 				paragraph_list.append(match)
@@ -87,7 +111,7 @@ def split_absatznr(text_list) -> List[str]:
 
 
 def iterate_files(directory, filetype):
-	for filename in sorted(os.listdir(directory))[:10]:
+	for filename in sorted(os.listdir(directory)):
 		if filename.endswith(filetype):
 			fname = os.path.join(directory, filename)
 			fname_json = os.path.join(directory, filename[:-5] + '.json')
@@ -105,8 +129,11 @@ def iterate_files(directory, filetype):
 					filter_list = filter(lambda x: x != "", text)  # removes empty list elements
 					filtered_paragraph_list = list(filter_list)
 					text_wo_hyphens = remove_hyphens(filtered_paragraph_list)
-					# print(text_list)
-					paragraph_list = split_absatznr(text_wo_hyphens)
+					# print(text_wo_hyphens)
+					# print('\n')
+					clean_text_list = remove_page_breaks(text_wo_hyphens)
+					# print(clean_text_list)
+					paragraph_list = split_absatznr(clean_text_list)
 
 					# building the xml tree
 					# text node with attributes
@@ -142,9 +169,9 @@ def iterate_files(directory, filetype):
 							para = para[1:]
 						p_node = ET.SubElement(body_node, 'p')
 						if re.search(absatz_pattern, para):
-							p_node.attrib['type'] = 'Absatznummer'
+							p_node.attrib['type'] = 'paragraph_mark'
 						else:
-							p_node.attrib['type'] = 'plain text'
+							p_node.attrib['type'] = 'plain_text'
 						p_node.text = para
 					# pb_node = ET.SubElement(body_node, 'pb') # drinlassen?
 					# footnote_node = ET.SubElement(text_node, 'footnote') # drinlassen?
@@ -152,7 +179,7 @@ def iterate_files(directory, filetype):
 					# creating/outputting the tree
 					tree = ET.ElementTree(text_node)
 					tree.write(full_save_name, encoding='UTF-8', xml_declaration=True)  # writes tree to file
-					# ET.dump(tree)  # shows tree in console
+					ET.dump(tree)  # shows tree in console
 					print('\n\n')
 
 
