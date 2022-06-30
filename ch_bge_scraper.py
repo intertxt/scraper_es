9a7b7660f61d143be01c73f0e218194a77a8d57e
intertxt <intertxt@protonmail.com>
2022-06-30 19:01:32 +0200

    year extraction added

--- ch_bge_scraper.py
+++ ch_bge_scraper.py
@@ -13,6 +13,7 @@ import unicodedata
 from duplicate_checker import get_duplicates
 # import dateparser ### install this
 
+# can be run with the following CLI prompt: python3.6 ch_bge_scraper.py -p /home/admin1/tb_tool/scraping_data/
 
 
 parser = argparse.ArgumentParser(description="generate clean XML-files from HTML-files")
@@ -32,6 +33,7 @@ absatz_pattern = r"^(\s)?[0-9]+(\.)?(\s)?([a-z]\)|([0-9]+(\.)?)*(\s-\s[0-9]+\.([
 absatz_pattern2 = r"^(\s)?[0-9]+\.([0-9]+(\.)?)*(\s-\s[0-9]+\.([0-9]+(\.)?)*)?\s-\s[0-9]+\.([0-9]+(\.)?)*(\s-\s[0-9]+\.([0-9]+(\.)?)*)?"
 absatz_pattern3 = r"([A-Z]([IVCMD]+)?(\.|\))-?(\s[a-z]\))?|(\s*[a-z]{1,3}(\)|\.))+|\d{1,3}((\.\s)|(\s[a-z]\)))|§.*:|[a-z]{1,2}\)(\s[a-z]{1,2}\))?|\d{1,3}\.)"
 datum_pattern = r"[0-9][0-9]?\.([\s]{1,2}([A-Z][a-z]+|März)|[0-9]{1,2}\.)\s?[1-9][0-9]{3}"
+code = r"^\d{1,3}\s[IVCMD]\s\d{1,3}$"
 false_marks = []
 
 
@@ -75,11 +77,15 @@ def get_pages(parsed_html) -> str:
 def get_paragraphs(text_wo_hyphens) -> List[str]:
 	"""Separate paragraph_marks from paragraphs."""
 	paragraph_list = []
+	year = ""
 	for i, elem in enumerate(text_wo_hyphens):
 		elem = elem.strip()
 		if elem.startswith("<table"):
 			paragraph_list.append(elem.strip())
 		else:
+			# if re.match(code, elem.strip()):
+			# 	paragraph_list.append(elem)
+			# 	year = str(1900 + int(elem.split()[0]) - 26)
 			if re.match(datum_pattern, elem):
 				paragraph_list.append(elem.strip())
 			elif re.match(absatz_pattern, elem):
@@ -103,7 +109,7 @@ def get_paragraphs(text_wo_hyphens) -> List[str]:
 				paragraph_list.append(elem.strip())
 	return paragraph_list
 
-def build_xml_tree(filename, loaded_json, pages, filter_list, full_save_name):
+def build_xml_tree(filename, loaded_json, pages, filter_list, full_save_name, year):
 	"""Build an XML-tree."""
 	text_node = ET.Element("text")
 	text_node.attrib["id"] = filename[:-5]
@@ -124,19 +130,19 @@ def build_xml_tree(filename, loaded_json, pages, filter_list, full_save_name):
 		text_node.attrib["language"] = loaded_json["Sprache"].replace('  ', ' ')
 	else:
 		text_node.attrib["language"] = loaded_json["Kopfzeile"][0]["Sprachen"].replace('  ', ' ')
-	if filename.endswith("nodate.html"):
-		text_node.attrib["date"] = "0000-00-00"
+	if filename.endswith("nodate.html") and year:
+		text_node.attrib["date"] = year+"-00-00"
 	else:
 		text_node.attrib["date"] = loaded_json["Datum"].replace('  ', ' ')
 	text_node.attrib["description"] = loaded_json["Abstract"][0]["Text"][12:]
 	text_node.attrib["type"] = loaded_json["Signatur"].replace('  ', ' ')
 	text_node.attrib["file"] = filename
 	if filename.endswith("nodate.html"):
-		text_node.attrib["year"] = "0000"
+		text_node.attrib["year"] = year
 	else:
 		text_node.attrib["year"] = loaded_json["Datum"][:4]
 	if filename.endswith("nodate.html"):
-		text_node.attrib["decade"] = "0000-00-00"
+		text_node.attrib["decade"] = year[:-1]+"0"
 	else:
 		text_node.attrib["decade"] = loaded_json["Datum"][:3] + "0"
 	if "HTML" in loaded_json.keys():
@@ -161,41 +167,46 @@ def build_xml_tree(filename, loaded_json, pages, filter_list, full_save_name):
 
 def iterate_files(directory, filetype):
 	duplicates = get_duplicates(directory) # from the nodate_duplicate_counter.py file
-	for filename in sorted(os.listdir(directory)):
-		if filename.endswith(filetype):
-			fname = os.path.join(directory, filename)
-			fname_json = os.path.join(directory, filename[:-5] + ".json")
-			if filename.endswith("nodate.html"):
-				xml_filename = filename.replace("nodate.html", "0000-00-00.xml")
-			else:
-				xml_filename = filename[:-5] + ".xml"
-			full_save_name = os.path.join(SAVE_PATH, xml_filename)
-			print("Current file name: ", os.path.abspath(fname), "will be converted into ", xml_filename)
-			print("\n")
-			with open(fname, "r", encoding="utf-8") as file:  # open html file for reading
-				with open(fname_json, "r", encoding="utf-8") as json_file:  # open json file for reading
-					loaded_json = json.load(json_file)  #load json
-					beautifulSoupText = BeautifulSoup(file.read(), "html.parser")  #read html
-					# print(beautifulSoupText)
-					text = parse_text(beautifulSoupText)
-					# print(text)
-					# print(beautifulSoupText)
-					# print("\n")
-					filter_list = list(filter(lambda x: x != "", text))
-					# print(filter_list)
-					text_with_pmarks = get_paragraphs(filter_list)
-					# print("\n")
-					# print(text_with_pmarks)
-					filter_list = list(filter(lambda x: x != "", text_with_pmarks))
-					print(filter_list)
-					filter_list = list(filter(lambda x: x != None, filter_list))
-					print(filter_list)
-					filter_list = list(filter(lambda x: x != ". ", filter_list))
-					pages = get_pages(beautifulSoupText)
-					tree = build_xml_tree(filename, loaded_json, pages, filter_list, full_save_name)
-					tree.write(full_save_name, encoding="UTF-8", xml_declaration=True)  # writes tree to file
-					ET.dump(tree)  # shows tree in console
-					print("\n\n")
+	with open("nodates.txt", "r", encoding="utf-8") as nodate_filenames:
+		nodate_name_list = [line[2:-14]+"nodate.html" for line in nodate_filenames.read().split("\n")]
+		for filename in sorted(os.listdir(directory)):
+			if filename.endswith(filetype) and "nodate" in filename and filename in nodate_name_list:
+				fname = os.path.join(directory, filename)
+				fname_json = os.path.join(directory, filename[:-5] + ".json")
+				print("Current file name: ", os.path.abspath(fname))
+				with open(fname, "r", encoding="utf-8") as file:  # open html file for reading
+					with open(fname_json, "r", encoding="utf-8") as json_file:  # open json file for reading
+						loaded_json = json.load(json_file)  #load json
+						beautifulSoupText = BeautifulSoup(file.read(), "html.parser")  #read html
+						# print(beautifulSoupText)
+						text = parse_text(beautifulSoupText)
+						# print(text)
+						# print(beautifulSoupText)
+						# print("\n")
+						filter_list = list(filter(lambda x: x != "", text))
+						# print(filter_list)
+						text_with_pmarks = get_paragraphs(filter_list)
+						# print("\n")
+						# print(text_with_pmarks)
+						filter_list = list(filter(lambda x: x != "", text_with_pmarks))
+						# print(filter_list)
+						filter_list = list(filter(lambda x: x != None, filter_list))
+						# print(filter_list)
+						filter_list = list(filter(lambda x: x != ". ", filter_list))
+						pages = get_pages(beautifulSoupText)
+						# calculate year from formula: 1900 + first number - 26
+						year = str(1900 + int(filename.split("_")[3].split("-")[1]) - 26)
+						if filename.endswith("nodate.html") and year:
+							xml_filename = filename.replace("nodate.html", year + "-00-00.xml")
+						else:
+							xml_filename = filename[:-5] + ".xml"
+						full_save_name = os.path.join(SAVE_PATH, xml_filename)
+						print("Current file name: ", os.path.abspath(fname), "will be converted into ", xml_filename)
+						print("\n")
+						tree = build_xml_tree(filename, loaded_json, pages, filter_list, full_save_name, year)
+						tree.write(full_save_name, encoding="UTF-8", xml_declaration=True)  # writes tree to file
+						ET.dump(tree)  # shows tree in console
+						print("\n\n")
 
 
 def main():
