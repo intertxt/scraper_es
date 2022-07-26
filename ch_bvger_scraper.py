30989e60af5036d57aae1e75fb17f804a813374f
intertxt <intertxt@protonmail.com>
2022-07-26 12:14:17 +0200

    only files w/ strange encoding missing

--- ch_bvger_scraper.py
+++ ch_bvger_scraper.py
@@ -308,42 +308,56 @@ def detect_lang(lines: List[str]) -> str:
 def main():
     footnotes = None
     pages = None
-    for filename in sorted(os.listdir(PATH_TO_DATA))[28780:]:
-        if filename.endswith("pdf"): # and "CH_BVGE_001_A-4995-2018_2019-05-06" in filename:
+    counter = 0
+    diff_encoded_files = ['CH_BVGE_001_A-6171-2009_2011-01-21.xml', 'CH_BVGE_001_BVGE-2007-10_2007-04-23.xml', 'CH_BVGE_001_BVGE-2007-11_2007-07-12.xml', 'CH_BVGE_001_BVGE-2007-12_2007-04-05.xml', 'CH_BVGE_001_BVGE-2007-13_2007-03-13.xml', 'CH_BVGE_001_BVGE-2007-14_2007-03-06.xml', 'CH_BVGE_001_BVGE-2007-15_2007-03-28.xml', 'CH_BVGE_001_BVGE-2007-16_2007-06-01.xml', 'CH_BVGE_001_BVGE-2007-17_2007-03-26.xml', 'CH_BVGE_001_BVGE-2007-18_2007-08-16.xml', 'CH_BVGE_001_BVGE-2007-19_2007-07-06.xml', 'CH_BVGE_001_BVGE-2007-1_2007-03-07.xml', 'CH_BVGE_001_BVGE-2007-21_2007-07-27.xml', 'CH_BVGE_001_BVGE-2007-22_2007-02-21.xml', 'CH_BVGE_001_BVGE-2007-23_2007-05-03.xml', 'CH_BVGE_001_BVGE-2007-24_2007-04-20.xml', 'CH_BVGE_001_BVGE-2007-25_2007-04-25.xml', 'CH_BVGE_001_BVGE-2007-26_2007-04-26.xml', 'CH_BVGE_001_BVGE-2007-27_2007-06-04.xml', 'CH_BVGE_001_BVGE-2007-28_2007-06-25.xml', 'CH_BVGE_001_BVGE-2007-29_2007-07-12.xml', 'CH_BVGE_001_BVGE-2007-2_2007-02-20.xml', 'CH_BVGE_001_BVGE-2007-30_2007-11-27.xml', 'CH_BVGE_001_BVGE-2007-31_2007-11-09.xml', 'CH_BVGE_001_BVGE-2007-32_2007-08-08.xml', 'CH_BVGE_001_BVGE-2007-33_2007-07-31.xml', 'CH_BVGE_001_BVGE-2007-34_2007-09-20.xml', 'CH_BVGE_001_BVGE-2007-35_2007-05-03.xml', 'CH_BVGE_001_BVGE-2007-36_2007-10-04.xml', 'CH_BVGE_001_BVGE-2007-37_2007-09-17.xml', 'CH_BVGE_001_BVGE-2007-38_2007-05-09.xml', 'CH_BVGE_001_BVGE-2007-39_2007-06-12.xml', 'CH_BVGE_001_BVGE-2007-3_2007-04-16.xml', 'CH_BVGE_001_BVGE-2007-40_2007-08-24.xml', 'CH_BVGE_001_BVGE-2007-41_2007-08-20.xml', 'CH_BVGE_001_BVGE-2007-42_2007-11-07.xml', 'CH_BVGE_001_BVGE-2007-43_2007-09-14.xml', 'CH_BVGE_001_BVGE-2007-44_2007-07-12.xml', 'CH_BVGE_001_BVGE-2007-45_2007-10-26.xml', 'CH_BVGE_001_BVGE-2007-46_2007-11-23.xml', 'CH_BVGE_001_BVGE-2007-47_2007-09-21.xml', 'CH_BVGE_001_BVGE-2007-48_2007-09-05.xml', 'CH_BVGE_001_BVGE-2007-49_2007-10-15.xml', 'CH_BVGE_001_BVGE-2007-4_2007-04-24.xml', 'CH_BVGE_001_BVGE-2007-50_2007-05-25.xml', 'CH_BVGE_001_BVGE-2007-5_2007-05-09.xml', 'CH_BVGE_001_BVGE-2007-6_2007-01-25.xml', 'CH_BVGE_001_BVGE-2007-7_2007-07-11.xml', 'CH_BVGE_001_BVGE-2007-8_2007-07-11.xml', 'CH_BVGE_001_BVGE-2007-9_2007-04-18.xml', 'CH_BVGE_001_BVGE-2008-10_2008-01-08.xml', 'CH_BVGE_001_BVGE-2008-11_2007-12-13.xml', 'CH_BVGE_001_BVGE-2008-12_2008-05-02.xml', 'CH_BVGE_001_BVGE-2008-13_2008-01-28.xml', 'CH_BVGE_001_BVGE-2008-14_2008-04-14.xml', 'CH_BVGE_001_BVGE-2008-15_2008-04-21.xml', 'CH_BVGE_001_BVGE-2008-16_2008-02-26.xml', 'CH_BVGE_001_BVGE-2008-17_2008-02-14.xml', 'CH_BVGE_001_BVGE-2008-18_2008-04-02.xml', 'CH_BVGE_001_BVGE-2008-19_2008-02-15.xml', 'CH_BVGE_001_BVGE-2008-1_2008-02-14.xml', 'CH_BVGE_001_BVGE-2008-20_2008-03-28.xml', 'CH_BVGE_001_BVGE-2008-21_2008-04-01.xml', 'CH_BVGE_001_BVGE-2008-22_2008-01-16.xml', 'CH_BVGE_001_BVGE-2008-23_2008-03-04.xml', 'CH_BVGE_001_BVGE-2008-24_2008-06-04.xml', 'CH_BVGE_001_BVGE-2008-25_2008-04-29.xml', 'CH_BVGE_001_BVGE-2008-26_2008-07-15.xml', 'CH_BVGE_001_BVGE-2008-27_2008-05-28.xml', 'CH_BVGE_001_BVGE-2008-28_2008-05-25.xml', 'CH_BVGE_001_BVGE-2008-29_2007-10-04.xml', 'CH_BVGE_001_BVGE-2008-2_2008-02-14.xml', 'CH_BVGE_001_BVGE-2008-30_2008-04-28.xml', 'CH_BVGE_001_BVGE-2008-31_2008-04-15.xml', 'CH_BVGE_001_BVGE-2008-32_2008-04-30.xml', 'CH_BVGE_001_BVGE-2008-33_2008-05-29.xml', 'CH_BVGE_001_BVGE-2008-34_2008-09-11.xml', 'CH_BVGE_001_BVGE-2008-35_2008-09-25.xml', 'CH_BVGE_001_BVGE-2008-36_2008-03-14.xml', 'CH_BVGE_001_BVGE-2008-37_2007-07-23.xml', 'CH_BVGE_001_BVGE-2008-38_2008-06-23.xml', 'CH_BVGE_001_BVGE-2008-39_2008-07-22.xml', 'CH_BVGE_001_BVGE-2008-3_2008-02-15.xml', 'CH_BVGE_001_BVGE-2008-40_2008-06-25.xml', 'CH_BVGE_001_BVGE-2008-41_2008-06-30.xml', 'CH_BVGE_001_BVGE-2008-42_2008-07-02.xml', 'CH_BVGE_001_BVGE-2008-43_2008-08-19.xml', 'CH_BVGE_001_BVGE-2008-44_2008-07-11.xml', 'CH_BVGE_001_BVGE-2008-45_2008-07-14.xml', 'CH_BVGE_001_BVGE-2008-46_2008-11-04.xml', 'CH_BVGE_001_BVGE-2008-47_2008-11-10.xml', 'CH_BVGE_001_BVGE-2008-48_2008-09-25.xml', 'CH_BVGE_001_BVGE-2008-49_2008-07-16.xml', 'CH_BVGE_001_BVGE-2008-4_2008-01-22.xml', 'CH_BVGE_001_BVGE-2008-50_2008-09-24.xml', 'CH_BVGE_001_BVGE-2008-51_2008-09-02.xml', 'CH_BVGE_001_BVGE-2008-52_2008-09-17.xml', 'CH_BVGE_001_BVGE-2008-53_2008-08-22.xml', 'CH_BVGE_001_BVGE-2008-54_2008-09-26.xml', 'CH_BVGE_001_BVGE-2008-55_2007-09-06.xml', 'CH_BVGE_001_BVGE-2008-56_2007-09-06.xml', 'CH_BVGE_001_BVGE-2008-57_2008-12-23.xml', 'CH_BVGE_001_BVGE-2008-58_2008-11-19.xml', 'CH_BVGE_001_BVGE-2008-59_2008-10-01.xml', 'CH_BVGE_001_BVGE-2008-5_2008-03-14.xml', 'CH_BVGE_001_BVGE-2008-60_2008-07-17.xml', 'CH_BVGE_001_BVGE-2008-61_2008-11-25.xml', 'CH_BVGE_001_BVGE-2008-62_2008-08-08.xml', 'CH_BVGE_001_BVGE-2008-63_2008-10-27.xml', 'CH_BVGE_001_BVGE-2008-64_2008-09-23.xml', 'CH_BVGE_001_BVGE-2008-65_2008-09-02.xml', 'CH_BVGE_001_BVGE-2008-66_2008-07-10.xml', 'CH_BVGE_001_BVGE-2008-6_2008-01-17.xml', 'CH_BVGE_001_BVGE-2008-7_2007-12-06.xml', 'CH_BVGE_001_BVGE-2008-8_2007-03-08.xml', 'CH_BVGE_001_BVGE-2008-9_2007-11-19.xml', 'CH_BVGE_001_BVGE-2009-10_2009-04-15.xml', 'CH_BVGE_001_BVGE-2009-11_2008-09-03.xml', 'CH_BVGE_001_BVGE-2009-12_2009-02-27.xml', 'CH_BVGE_001_BVGE-2009-13_2009-03-06.xml', 'CH_BVGE_001_BVGE-2009-14_2008-10-10.xml', 'CH_BVGE_001_BVGE-2009-15_2009-03-11.xml', 'CH_BVGE_001_BVGE-2009-16_2009-05-05.xml', 'CH_BVGE_001_BVGE-2009-17_2009-02-13.xml', 'CH_BVGE_001_BVGE-2009-18_2009-05-20.xml', 'CH_BVGE_001_BVGE-2009-19_2009-07-02.xml', 'CH_BVGE_001_BVGE-2009-1_2008-01-30.xml', 'CH_BVGE_001_BVGE-2009-20_2009-03-23.xml', 'CH_BVGE_001_BVGE-2009-21_2009-04-15.xml', 'CH_BVGE_001_BVGE-2009-22_2009-02-02.xml', 'CH_BVGE_001_BVGE-2009-23_2009-05-25.xml', 'CH_BVGE_001_BVGE-2009-24_2009-05-29.xml', 'CH_BVGE_001_BVGE-2009-25_2009-03-12.xml', 'CH_BVGE_001_BVGE-2009-26_2009-05-15.xml', 'CH_BVGE_001_BVGE-2009-27_2009-04-06.xml', 'CH_BVGE_001_BVGE-2009-28_2009-07-09.xml', 'CH_BVGE_001_BVGE-2009-29_2009-10-07.xml', 'CH_BVGE_001_BVGE-2009-2_2008-08-07.xml', 'CH_BVGE_001_BVGE-2009-30_2009-03-04.xml', 'CH_BVGE_001_BVGE-2009-31_2009-04-30.xml', 'CH_BVGE_001_BVGE-2009-32_2009-07-31.xml', 'CH_BVGE_001_BVGE-2009-33_2009-05-19.xml', 'CH_BVGE_001_BVGE-2009-34_2009-08-21.xml', 'CH_BVGE_001_BVGE-2009-35_2009-02-12.xml', 'CH_BVGE_001_BVGE-2009-36_2009-02-03.xml', 'CH_BVGE_001_BVGE-2009-37_2008-06-18.xml', 'CH_BVGE_001_BVGE-2009-38_2009-09-14.xml', 'CH_BVGE_001_BVGE-2009-39_2009-07-02.xml', 'CH_BVGE_001_BVGE-2009-3_2009-01-08.xml', 'CH_BVGE_001_BVGE-2009-4_2008-06-09.xml', 'CH_BVGE_001_BVGE-2009-5_2008-08-18.xml', 'CH_BVGE_001_BVGE-2009-6_2008-11-25.xml', 'CH_BVGE_001_BVGE-2009-7_2008-12-16.xml', 'CH_BVGE_001_BVGE-2009-8_2009-05-12.xml', 'CH_BVGE_001_BVGE-2009-9_2008-12-22.xml']
+
+    # used this with adjusted path to get diff_encoded_files list
+    # for filename in sorted(os.listdir(PATH_TO_DATA)):
+    #     with open(os.path.join(PATH_TO_DATA, filename), "r") as file:
+    #         if "���" in file.read():
+    #             counter += 1
+    #             diff_encoded_files.append(filename)
+    # print(diff_encoded_files)
+    # print(counter)
+
+    for filename in sorted(os.listdir(PATH_TO_DATA)):
+        if filename.endswith("pdf") and filename[:-4]+".xml" in diff_encoded_files: # and "CH_BVGE_001_A-4995-2018_2019-05-06" in filename:
             print(f"The following file is being processed:\n{os.path.join(PATH_TO_DATA, filename)}\n")
 
             # parse with tika library from separate script
             parsed_text = tika_parse(os.path.join(PATH_TO_DATA, filename))
+            
+            ### stopped here parsing the strangely encoded files
 
-            lines = split_lines(parsed_text)
-            pages = get_pages(lines)
-            # footnotes = get_footnotes(lines)
-            clean_text = get_paras(lines)
-
-            # create new filenames for the xml files
-            if filename.endswith("nodate.html"):
-                xml_filename = filename.replace("nodate.html", "0000-00-00.xml")
-            else:
-                xml_filename = filename[:-4] + ".xml"
-
-            # open json pendant
-            json_name = filename[:-3]+"json"
-            if json_name in sorted(os.listdir(PATH_TO_DATA)):
-                with open(os.path.join(PATH_TO_DATA, json_name), "r", encoding="utf-8") as json_file:
-                    loaded_json = json.load(json_file)  # load json
-                    tree = build_xml_tree(filename, loaded_json, clean_text, footnotes, pages)  # generates XML treea
-                    tree.write(os.path.join(SAVE_PATH, xml_filename), encoding="UTF-8", xml_declaration=True)  # writes tree to file
-                    ET.dump(tree)  # shows tree in console
+            # lines = split_lines(parsed_text)
+            # pages = get_pages(lines)
+            # # footnotes = get_footnotes(lines)
+            # clean_text = get_paras(lines)
+            #
+            # # create new filenames for the xml files
+            # if filename.endswith("nodate.html"):
+            #     xml_filename = filename.replace("nodate.html", "0000-00-00.xml")
+            # else:
+            #     xml_filename = filename[:-4] + ".xml"
+            #
+            # # open json pendant
+            # json_name = filename[:-3]+"json"
+            # if json_name in sorted(os.listdir(PATH_TO_DATA)):
+            #     with open(os.path.join(PATH_TO_DATA, json_name), "r", encoding="utf-8") as json_file:
+            #         loaded_json = json.load(json_file)  # load json
+            #         tree = build_xml_tree(filename, loaded_json, clean_text, footnotes, pages)  # generates XML treea
+            #         tree.write(os.path.join(SAVE_PATH, xml_filename), encoding="UTF-8", xml_declaration=True)  # writes tree to file
+            #         ET.dump(tree)  # shows tree in console
 
             # detect_lang(lines)
-            # print(parsed_text)
+            print(parsed_text)
             # print(lines)
             # print(pages)
             # for _ in footnotes:
             #     print(f"{_}\t{footnotes[_]}")
             # print(footnotes)
             # print(clean_text)
-
+            # break
 
 
             print("\n===========================================================\n\n")
