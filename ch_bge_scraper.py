89f81a0166b62584cc5ff641dc5cff28b2499525
intertxt <intertxt@protonmail.com>
2023-01-19 14:28:47 +0100

    adjusted segmentation

--- ch_bge_scraper.py
+++ ch_bge_scraper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-# scraper for BS_Omni
+# scraper for CH_BGE
 
 from bs4 import BeautifulSoup, NavigableString, Tag
 import os
@@ -11,9 +11,12 @@ import json
 import re
 import unicodedata
 from duplicate_checker import get_duplicates
+from html import unescape
+
 # import dateparser ### install this
 
-# can be run with the following CLI prompt: python3.6 ch_bge_scraper.py -p /home/admin1/tb_tool/scraping_data/
+# can be run with the following CLI prompt:
+# python3.6 ch_bge_scraper.py -p /home/admin1/tb_tool/scraping_data/
 
 
 parser = argparse.ArgumentParser(description="generate clean XML-files from HTML-files")
@@ -29,8 +32,12 @@ SAVE_PATH = "/home/admin1/tb_tool/clean_scraper_data/CH_BGE_clean/"
 
 ALLOWED_CLASSES = [['big', 'bold'], "paraatf", "bold", ['center', 'pagebreak'], "artref", "bgeref_id"]
 
-absatz_pattern = r"^(\s)?[0-9]+(\.)?(\s)?([a-z]\)|([0-9]+(\.)?)*(\s-\s[0-9]+\.([0-9]+(\.)?)*)?)"
-absatz_pattern2 = r"^(\s)?[0-9]+\.([0-9]+(\.)?)*(\s-\s[0-9]+\.([0-9]+(\.)?)*)?\s-\s[0-9]+\.([0-9]+(\.)?)*(\s-\s[0-9]+\.([0-9]+(\.)?)*)?"
+# absatz_pattern = r"^(\s)?[0-9]+(\.)?(\s)?([a-z]\)|([0-9]+(\.)?)*(\s-\s[0-9]+\.([0-9]+(\.)?)*)?)"
+# absatz_pattern2 = r"^(\s)?[0-9]+\.([0-9]+(\.)?)*(\s-\s[0-9]+\.([0-9]+(\.)?)*)?\s-\s[0-9]+\.([0-9]+(\.)?)*(\s-\s[0-9]+\.([0-9]+(\.)?)*)?"
+# absatz_pattern3 = r"([A-Z]([IVCMD]+)?(\.|\))-?(\s[a-z]\))?|(\s*[a-z]{1,3}(\)|\.))+|\d{1,3}((\.\s)|(\s[a-z]\)))|§.*:|[a-z]{1,2}\)(\s[a-z]{1,2}\))?|\d{1,3}\.)"
+# datum_pattern = r"[0-9][0-9]?\.([\s]{1,2}([A-Z][a-z]+|März)|[0-9]{1,2}\.)\s?[1-9][0-9]{3}"
+absatz_pattern = r"^(\s)?[0-9]+(\.)?(\s)?([a-z]\)|([0-9]+(\.)?)*(\s-\s[0-9]+\.([0-9]+(\.)?)*)?)(\s?[a-i]\))?"
+absatz_pattern2 = r"^([a-i]\))?((\s)?[0-9]+\.([0-9]+(\.)?)*(\s-\s[0-9]+\.([0-9]+(\.)?)*)?\s-\s[0-9]+\.([0-9]+(\.)?)*(\s-\s[0-9]+\.([0-9]+(\.)?)*)?)?"
 absatz_pattern3 = r"([A-Z]([IVCMD]+)?(\.|\))-?(\s[a-z]\))?|(\s*[a-z]{1,3}(\)|\.))+|\d{1,3}((\.\s)|(\s[a-z]\)))|§.*:|[a-z]{1,2}\)(\s[a-z]{1,2}\))?|\d{1,3}\.)"
 datum_pattern = r"[0-9][0-9]?\.([\s]{1,2}([A-Z][a-z]+|März)|[0-9]{1,2}\.)\s?[1-9][0-9]{3}"
 code = r"^\d{1,3}\s[IVCMD]\s\d{1,3}$"
@@ -46,7 +53,7 @@ def parse_text(parsed_html) -> List[str]:
 			check = any(item in tag["class"] for item in ALLOWED_CLASSES)
 			if check:
 				tag_text = unicodedata.normalize("NFKD", tag.get_text().strip().replace("\n", ""))
-				text.append(tag_text)
+				text.append(unescape(tag_text))
 		else: # extracts text in between breaks
 			next_s = tag.nextSibling
 			if not (next_s and isinstance(next_s, NavigableString)):
@@ -54,7 +61,7 @@ def parse_text(parsed_html) -> List[str]:
 			next2_s = next_s.nextSibling
 			if next2_s and isinstance(next2_s, Tag) and next2_s.name == 'br':
 				br_text = unicodedata.normalize("NFKD", next_s.strip())
-				text.append(br_text)
+				text.append(unescape(br_text))
 	return text
 
 
@@ -71,7 +78,10 @@ def get_pages(parsed_html) -> str:
 		if "name" in tag.attrs:
 			if tag["name"].startswith("page"):
 				page_list.append(tag["name"].lstrip("page"))
-	return page_list[0]+"–"+page_list[-1]
+	if len(page_list) > 0:
+		return page_list[0]+"–"+page_list[-1]
+	else:
+		return ""
 
 
 def get_paragraphs(text_wo_hyphens) -> List[str]:
@@ -79,34 +89,43 @@ def get_paragraphs(text_wo_hyphens) -> List[str]:
 	paragraph_list = []
 	year = ""
 	for i, elem in enumerate(text_wo_hyphens):
-		elem = elem.strip()
-		if elem.startswith("<table"):
-			paragraph_list.append(elem.strip())
-		else:
-			# if re.match(code, elem.strip()):
-			# 	paragraph_list.append(elem)
-			# 	year = str(1900 + int(elem.split()[0]) - 26)
-			if re.match(datum_pattern, elem):
+		if elem:
+			elem = elem.strip()
+			if elem.startswith("<table"):
 				paragraph_list.append(elem.strip())
-			elif re.match(absatz_pattern, elem):
-				match = re.match(absatz_pattern, elem).group(0)
-				if elem.startswith(match + "__") or elem.startswith(match + "Abteilung") or elem.startswith(
-						match + "Kammer") or elem.startswith(match + "Auszug"):
-					paragraph_list.append(elem.strip())
-				else:
-					paragraph_list.append(match.strip())
-					paragraph_list.append(elem[len(match):].strip())
-			elif re.match(absatz_pattern3, elem):
-				match = re.match(absatz_pattern3, elem).group(0)
-				if elem.startswith(match + "__") or elem.startswith(match + "Abteilung") or elem.startswith(
-						match + "Kammer") or text_wo_hyphens[i] == text_wo_hyphens[-1] or elem.startswith(
-					match + "2"):
-					paragraph_list.append(elem.strip())
-				else:
-					paragraph_list.append(match.strip())
-					paragraph_list.append(elem[len(match):].strip())
 			else:
-				paragraph_list.append(elem.strip())
+				# if re.match(code, elem.strip()):
+				# 	paragraph_list.append(elem)
+				# 	year = str(1900 + int(elem.split()[0]) - 26)
+				if re.match(datum_pattern, elem):
+					paragraph_list.append(elem.strip())
+				elif re.match(absatz_pattern, elem):
+					match = re.match(absatz_pattern, elem).group(0)
+					if elem.startswith(match + "__") or elem.startswith(match + "Abteilung") or elem.startswith(
+							match + "Kammer") or elem.startswith(match + "Auszug") or (elem[len(match):].strip() and elem[len(match):].strip()[0].isupper()):
+						paragraph_list.append(elem.strip())
+					else:
+						paragraph_list.append(match.strip())
+						paragraph_list.append(elem[len(match):].strip())
+				elif re.match(absatz_pattern2, elem):
+					match = re.match(absatz_pattern2, elem).group(0)
+					if elem.startswith(match + "__") or elem.startswith(match + "Abteilung") or elem.startswith(
+							match + "Kammer") or elem.startswith(match + "Auszug"):
+						paragraph_list.append(elem.strip())
+					else:
+						paragraph_list.append(match.strip())
+						paragraph_list.append(elem[len(match):].strip())
+				elif re.match(absatz_pattern3, elem) and elem.split()[1].strip()[0].isupper():
+					match = re.match(absatz_pattern3, elem).group(0)
+					if elem.startswith(match + "__") or elem.startswith(match + "Abteilung") or elem.startswith(
+							match + "Kammer") or text_wo_hyphens[i] == text_wo_hyphens[-1] or elem.startswith(
+						match + "2"):
+						paragraph_list.append(elem.strip())
+					else:
+						paragraph_list.append(match.strip())
+						paragraph_list.append(elem[len(match):].strip())
+				else:
+					paragraph_list.append(elem.strip())
 	return paragraph_list
 
 def build_xml_tree(filename, loaded_json, pages, filter_list, full_save_name, year):
@@ -154,11 +173,17 @@ def build_xml_tree(filename, loaded_json, pages, filter_list, full_save_name, ye
 		if para.startswith(" "):  # so that random whitespaces in the beginning of paragraphs are deleted
 			para = para[1:]
 		p_node = ET.SubElement(body_node, "p")
-		if re.fullmatch(absatz_pattern3, para):  # changed to fullmatch seemed better
+		if para in false_marks:
+			p_node.attrib["type"] = "plain_text"
+		elif re.match(datum_pattern, para):
+			p_node.attrib["type"] = "plain_text"
+		elif re.fullmatch(absatz_pattern3, para) or re.fullmatch(absatz_pattern2, para) or re.fullmatch(absatz_pattern, para):
 			p_node.attrib["type"] = "paragraph_mark"
+		elif para.startswith("<table"):
+			p_node.attrib["type"] = "table"
 		else:
 			p_node.attrib["type"] = "plain_text"
-		p_node.text = para
+		p_node.text = unescape(para.strip())
 	# pb_node = ET.SubElement(body_node, "pb") # drinlassen?
 	# footnote_node = ET.SubElement(text_node, "footnote") # drinlassen?
 	tree = ET.ElementTree(text_node) # creating the tree
@@ -166,47 +191,48 @@ def build_xml_tree(filename, loaded_json, pages, filter_list, full_save_name, ye
 
 
 def iterate_files(directory, filetype):
+	files = ["CH_BGE_002_BGE-109-Ia-248_", "CH_BGE_007_BGE-126-V-193_"]
 	duplicates = get_duplicates(directory) # from the nodate_duplicate_counter.py file
-	with open("nodates.txt", "r", encoding="utf-8") as nodate_filenames:
-		nodate_name_list = [line[2:-14]+"nodate.html" for line in nodate_filenames.read().split("\n")]
-		for filename in sorted(os.listdir(directory)):
-			if filename.endswith(filetype) and "nodate" in filename and filename in nodate_name_list:
-				fname = os.path.join(directory, filename)
-				fname_json = os.path.join(directory, filename[:-5] + ".json")
-				print("Current file name: ", os.path.abspath(fname))
-				with open(fname, "r", encoding="utf-8") as file:  # open html file for reading
-					with open(fname_json, "r", encoding="utf-8") as json_file:  # open json file for reading
-						loaded_json = json.load(json_file)  #load json
-						beautifulSoupText = BeautifulSoup(file.read(), "html.parser")  #read html
-						# print(beautifulSoupText)
-						text = parse_text(beautifulSoupText)
-						# print(text)
-						# print(beautifulSoupText)
-						# print("\n")
-						filter_list = list(filter(lambda x: x != "", text))
-						# print(filter_list)
-						text_with_pmarks = get_paragraphs(filter_list)
-						# print("\n")
-						# print(text_with_pmarks)
-						filter_list = list(filter(lambda x: x != "", text_with_pmarks))
-						# print(filter_list)
-						filter_list = list(filter(lambda x: x != None, filter_list))
-						# print(filter_list)
-						filter_list = list(filter(lambda x: x != ". ", filter_list))
-						pages = get_pages(beautifulSoupText)
-						# calculate year from formula: 1900 + first number - 26
-						year = str(1900 + int(filename.split("_")[3].split("-")[1]) - 26)
-						if filename.endswith("nodate.html") and year:
-							xml_filename = filename.replace("nodate.html", year + "-00-00.xml")
-						else:
-							xml_filename = filename[:-5] + ".xml"
-						full_save_name = os.path.join(SAVE_PATH, xml_filename)
-						print("Current file name: ", os.path.abspath(fname), "will be converted into ", xml_filename)
-						print("\n")
-						tree = build_xml_tree(filename, loaded_json, pages, filter_list, full_save_name, year)
-						tree.write(full_save_name, encoding="UTF-8", xml_declaration=True)  # writes tree to file
-						ET.dump(tree)  # shows tree in console
-						print("\n\n")
+	# with open("nodates.txt", "r", encoding="utf-8") as nodate_filenames:
+	# 	nodate_name_list = [line[2:-14]+"nodate.html" for line in nodate_filenames.read().split("\n")]
+	for filename in sorted(os.listdir(directory)):
+		#if filename.endswith(filetype) and "nodate" in filename and filename.strip("nodate.html") in files:
+			fname = os.path.join(directory, filename)
+			fname_json = os.path.join(directory, filename[:-5] + ".json")
+			print("Current file name: ", os.path.abspath(fname))
+			with open(fname, "r", encoding="utf-8") as file:  # open html file for reading
+				with open(fname_json, "r", encoding="utf-8") as json_file:  # open json file for reading
+					loaded_json = json.load(json_file)  #load json
+					beautifulSoupText = BeautifulSoup(file.read(), "html.parser")  #read html
+					# print(beautifulSoupText)
+					text = parse_text(beautifulSoupText)
+					# print(text)
+					# print(beautifulSoupText)
+					# print("\n")
+					filter_list = list(filter(lambda x: x != "", text))
+					# print(filter_list)
+					text_with_pmarks = get_paragraphs(filter_list)
+					# print("\n")
+					# print(text_with_pmarks)
+					filter_list = list(filter(lambda x: x != "", text_with_pmarks))
+					# print(filter_list)
+					filter_list = list(filter(lambda x: x != None, filter_list))
+					# print(filter_list)
+					filter_list = list(filter(lambda x: x != ". ", filter_list))
+					pages = get_pages(beautifulSoupText)
+					# calculate year from formula: 1900 + first number - 26
+					year = str(1900 + int(filename.split("_")[3].split("-")[1]) - 26)
+					if filename.endswith("nodate.html") and year:
+						xml_filename = filename.replace("nodate.html", year + "-00-00.xml")
+					else:
+						xml_filename = filename[:-5] + ".xml"
+					full_save_name = os.path.join(SAVE_PATH, xml_filename)
+					print("Current file name: ", os.path.abspath(fname), "will be converted into ", xml_filename)
+					print("\n")
+					tree = build_xml_tree(filename, loaded_json, pages, filter_list, full_save_name, year)
+					tree.write(full_save_name, encoding="UTF-8", xml_declaration=True)  # writes tree to file
+					ET.dump(tree)  # shows tree in console
+					print("\n\n")
 
 
 def main():
