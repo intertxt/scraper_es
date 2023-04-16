d0631a218d202252ec558f16b4939f07090d3a76
intertxt <sur@srv-app-440.zhaw.ch>
2023-04-16 18:57:13 +0200

    added start folder choise arg

--- crawler/crawling.py
+++ crawler/crawling.py
@@ -20,12 +20,15 @@ print(response)  ## 200 is good -> macht pause damit der server den scraper nich
 
 counter = {}
 
+
 def parse_arguments():
     """
     Argument Parser implementation.
     """
     parser = argparse.ArgumentParser(description="Crawl files from entscheidsuche.ch.")
-    parser.add_argument("path", help="Add a path to the directory that contains the directories for all cantonal documents." )
+    parser.add_argument("--path", "-p", help="Add a path to the directory that contains the directories for all cantonal documents." )
+    parser.add_argument("--specific_path", "-sp", help="Add a path to the directory that contains documents of one canton/court."
+                                                       "Continues with further directories in alphabetical order." )
     return parser
 
 
@@ -38,19 +41,26 @@ def isDirectory(url):
         return False
 
 
-def findLinks(url, path):
-    os.chdir("/")
-    if not os.path.exists(path):
-        print(f"This path seems to be wrong.")
-        quit()
+def findLinks(url, path, c):
     page = requests.get(url).content
     bsObj = BeautifulSoup(page, "html.parser")
     potential_dirs = bsObj.findAll("a", href=True)  # findet alle links in der aufgerufenen seite
+    # print(potential_dirs)
+    # quit()
+
     for link in potential_dirs:
-        if isDirectory(link["href"]):  # falls es ein directory ist wird es neu aufgerufen (rekursiv)
+        if isDirectory(link["href"]) and not path.endswith("data") and c == 1:  # falls es ein directory ist wird es neu aufgerufen (rekursiv)
+            canton = path.split("/")[-1]
+            if len(link["href"]) != 1 and link["href"] != "/docs/" and canton in link["href"]:
+                newUrl = url + link["href"].lstrip("/docs")
+                c += 1
+                findLinks(newUrl, path, c)
+
+        elif isDirectory(link["href"]):  # falls es ein directory ist wird es neu aufgerufen (rekursiv)
             if len(link["href"]) != 1 and link["href"] != "/docs/":
                 newUrl = url + link["href"].lstrip("/docs")
                 findLinks(newUrl, path)
+
         else:
             if link["href"].endswith(".json") or link["href"].endswith(".html") or link["href"].endswith(".pdf"):
                 print(link["href"])
@@ -79,15 +89,27 @@ def findLinks(url, path):
 
 def main():
     args = parse_arguments().parse_args()
-    findLinks(url, args.path)
+
+    if args.path:
+        path = args.path
+    elif args.specific_path:
+        path = args.specific_path
+    else:
+        raise Warning("Please enter path!")
+
+    os.chdir("/")
+    if not os.path.exists(path):
+        print(f"This path seems to be wrong.")
+        quit()
+
+    c = 1
+    findLinks(url, path, c)
+
     print(counter)  # analyse wie viele files gefunden wurden pro directory
 
 
 if __name__ == '__main__':
     main()
-
-
-
 
 
 
